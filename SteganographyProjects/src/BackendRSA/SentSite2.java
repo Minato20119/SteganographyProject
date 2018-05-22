3250bfa8216250e43697efc9acbb93fa2f31cb55
Minato20119 <31340334+Minato20119@users.noreply.github.com>
2018-05-22 07:09:55 +0700

    Update SentSite2.java

--- SteganographyProjects/src/BackendRSA/SentSite2.java
+++ SteganographyProjects/src/BackendRSA/SentSite2.java
@@ -21,234 +21,358 @@ import javax.imageio.ImageIO;
  */
 public class SentSite2 {
 
-	private static BufferedReader bufferedReader;
+    private static BufferedReader bufferedReader;
 
-	// Input image
-	public static BufferedImage inputImage(String imageFilePath) {
-		BufferedImage image = null;
-		try {
+    // Input image
+    public static BufferedImage inputImage(String imageFilePath) {
+        BufferedImage image = null;
+        try {
 
-			File imageFile = new File(imageFilePath);
-			image = ImageIO.read(imageFile);
+            File imageFile = new File(imageFilePath);
+            image = ImageIO.read(imageFile);
 
-		} catch (IOException e) {
-			System.out.println("Error input image: " + e);
-		}
-		return image;
-	}
+        } catch (IOException e) {
+            System.out.println("Error input image: " + e);
+        }
+        return image;
+    }
 
-	// Divide the image into 8 blocks
-	public static int[] divideImageTo8Blocks(BufferedImage image) {
+    // Divide the image into 8 blocks
+    public static int[] divideImageTo8Blocks(BufferedImage image) {
 
-		int height = image.getHeight();
+        int height = image.getHeight();
 
-		int temp = (height - 80) / 8;
-		int result = 0;
+        if (height < 88) {
+            System.exit(0);
+        }
 
-		// 8 block + key K1 and K2
-		int[] milestones = new int[10];
-		
-		// milestones[0] = 0;
+        int temp = (height - 80) / 8;
+        int result = 0;
 
-		for (int i = 0; i < milestones.length; i++) {
+        // 8 block + key K1 and K2
+        int[] milestones = new int[10];
 
-			if (i == 1) {
-				result += 80;
-				milestones[i + 1] = result;
-				continue;
-			}
+        // milestones[0] = 0;
+        for (int i = 0; i < milestones.length; i++) {
 
-			if (i == 8) {
-				result += height - result;
-				milestones[i + 1] = result;
-				break;
-			}
+            if (i == 1) {
+                result += 80;
+                milestones[i + 1] = result;
+                continue;
+            }
 
-			result += temp;
-			milestones[i + 1] = result;
-		}
+            if (i == 8) {
+                result += height - result;
+                milestones[i + 1] = result;
+                break;
+            }
 
-		return milestones;
-	}
+            result += temp;
+            milestones[i + 1] = result;
+        }
 
-	// Read text file
-	public static String inputTextFile(String pathFileText) throws IOException {
+        return milestones;
+    }
 
-		FileReader fileReader = null;
-		String plainText = "";
+    // Read text file
+    public static String inputTextFile(String pathFileText) throws IOException {
 
-		try {
-			fileReader = new FileReader(pathFileText);
-			bufferedReader = new BufferedReader(fileReader);
+        FileReader fileReader = null;
+        String plainText = "";
 
-			String readLine;
-			while ((readLine = bufferedReader.readLine()) != null) {
-				plainText += readLine + "\n";
-			}
+        try {
+            fileReader = new FileReader(pathFileText);
+            bufferedReader = new BufferedReader(fileReader);
 
-		} catch (IOException e) {
-			System.out.println("Error input text file: " + e);
-		} finally {
+            String readLine;
+            while ((readLine = bufferedReader.readLine()) != null) {
+                plainText += readLine + "\n";
+            }
 
-			if (bufferedReader != null) {
-				bufferedReader.close();
-			}
+        } catch (IOException e) {
+            System.out.println("Error input text file: " + e);
+        } finally {
 
-			if (fileReader != null) {
-				fileReader.close();
-			}
-		}
-		return plainText;
-	}
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
 
-	// Convert text to binary
-	public static String convertTextToBinary(String cipherText) {
+            if (fileReader != null) {
+                fileReader.close();
+            }
+        }
+        return plainText;
+    }
 
-		byte[] bytes = cipherText.getBytes();
-		StringBuilder binary = new StringBuilder();
+    // Convert text to binary
+    public static String convertTextToBinary(String cipherText) {
 
-		for (byte b : bytes) {
-			int value = b;
+        byte[] bytes = cipherText.getBytes();
+        StringBuilder binary = new StringBuilder();
 
-			for (int i = 0; i < 8; i++) {
-				binary.append((value & 128) == 0 ? 0 : 1);
-				value <<= 1;
-			}
-		}
+        for (byte b : bytes) {
+            int value = b;
 
-		return binary.toString();
-	}
+            for (int i = 0; i < 8; i++) {
+                binary.append((value & 128) == 0 ? 0 : 1);
+                value <<= 1;
+            }
+        }
 
-	// Divide the cipher text into 8 blocks
-	public static String[] divideCipherTextTo8Blocks(String binaryCipherText) {
+        return binary.toString();
+    }
 
-		String[] arrayBinaryCipherText = new String[8];
+    // Divide the cipher text into 8 blocks
+    public static String[] divideCipherTextTo8Blocks(String binaryCipherText) {
 
-		for (int i = 0; i < arrayBinaryCipherText.length; i++) {
-			arrayBinaryCipherText[i] = "";
-		}
+        String[] arrayBinaryCipherText = new String[8];
 
-		for (int i = 0; i < binaryCipherText.length(); i++) {
+        for (int i = 0; i < arrayBinaryCipherText.length; i++) {
+            arrayBinaryCipherText[i] = "";
+        }
 
-			// bit 0 become M0
-			if (i % 8 == 0) {
-				arrayBinaryCipherText[0] += String.valueOf(binaryCipherText.charAt(i));
-				continue;
-			}
+        for (int i = 0; i < binaryCipherText.length(); i++) {
 
-			// bit 1 become M1
-			if (i % 8 == 1) {
-				arrayBinaryCipherText[1] += String.valueOf(binaryCipherText.charAt(i));
-				continue;
-			}
+            // bit 0 become M0
+            if (i % 8 == 0) {
+                arrayBinaryCipherText[0] += String.valueOf(binaryCipherText.charAt(i));
+                continue;
+            }
 
-			// bit 2 become M2
-			if (i % 8 == 2) {
-				arrayBinaryCipherText[2] += String.valueOf(binaryCipherText.charAt(i));
-				continue;
-			}
+            // bit 1 become M1
+            if (i % 8 == 1) {
+                arrayBinaryCipherText[1] += String.valueOf(binaryCipherText.charAt(i));
+                continue;
+            }
 
-			// bit 3 become M3
-			if (i % 8 == 3) {
-				arrayBinaryCipherText[3] += String.valueOf(binaryCipherText.charAt(i));
-				continue;
-			}
+            // bit 2 become M2
+            if (i % 8 == 2) {
+                arrayBinaryCipherText[2] += String.valueOf(binaryCipherText.charAt(i));
+                continue;
+            }
 
-			// bit 4 become M4
-			if (i % 8 == 4) {
-				arrayBinaryCipherText[4] += String.valueOf(binaryCipherText.charAt(i));
-				continue;
-			}
+            // bit 3 become M3
+            if (i % 8 == 3) {
+                arrayBinaryCipherText[3] += String.valueOf(binaryCipherText.charAt(i));
+                continue;
+            }
 
-			// bit 5 become M5
-			if (i % 8 == 5) {
-				arrayBinaryCipherText[5] += String.valueOf(binaryCipherText.charAt(i));
-				continue;
-			}
+            // bit 4 become M4
+            if (i % 8 == 4) {
+                arrayBinaryCipherText[4] += String.valueOf(binaryCipherText.charAt(i));
+                continue;
+            }
 
-			// bit 6 become M6
-			if (i % 8 == 6) {
-				arrayBinaryCipherText[6] += String.valueOf(binaryCipherText.charAt(i));
-				continue;
-			}
+            // bit 5 become M5
+            if (i % 8 == 5) {
+                arrayBinaryCipherText[5] += String.valueOf(binaryCipherText.charAt(i));
+                continue;
+            }
 
-			// bit 7 become M7
-			if (i % 8 == 7) {
-				arrayBinaryCipherText[7] += String.valueOf(binaryCipherText.charAt(i));
-			}
-		}
+            // bit 6 become M6
+            if (i % 8 == 6) {
+                arrayBinaryCipherText[6] += String.valueOf(binaryCipherText.charAt(i));
+                continue;
+            }
 
-		return arrayBinaryCipherText;
+            // bit 7 become M7
+            if (i % 8 == 7) {
+                arrayBinaryCipherText[7] += String.valueOf(binaryCipherText.charAt(i));
+            }
+        }
 
-	}
+        return arrayBinaryCipherText;
 
-	// Create K1 key random
-	public static String createK1Key() {
+    }
 
-		String keyRandom = "";
+    // Create K1 key random
+    public static String createK1Key() {
 
-		ArrayList<Integer> numbersRandom = new ArrayList<Integer>();
+        String keyRandom = "";
 
-		for (int i = 0; i < 8; i++) {
-			numbersRandom.add(i);
-		}
-		Collections.shuffle(numbersRandom);
+        ArrayList<Integer> numbersRandom = new ArrayList<>();
 
-		for (int i = 0; i < numbersRandom.size(); i++) {
-			keyRandom += numbersRandom.get(i);
-		}
+        for (int i = 0; i < 8; i++) {
+            numbersRandom.add(i);
+        }
+        Collections.shuffle(numbersRandom);
 
-		return keyRandom;
-	}
+        for (int i = 0; i < numbersRandom.size(); i++) {
+            keyRandom += numbersRandom.get(i);
+        }
 
-	public static void findK2Key(BufferedImage image) {
+        return keyRandom;
+    }
 
-		int[] milestones = divideImageTo8Blocks(image);
-		
+    // Find K2 key
+    public static String findK2Key(BufferedImage image, int[] milestones) {
 
-		int height = 0;
-		
-		for (int i = 0; i < 9; i++) {
-			
-			// because byte du tru
-			if (i == 1) {
-				continue;
-			}
-			
-			for (height = milestones[i]; height <= milestones[i + 1]; height++) {
-				for (int width = 0; width < image.getWidth(); width++) {
-					int rgbValue = image.getRGB(width, height);
-					
-					
-				}
-			}
-		}
-	}
+        String keyK2 = "";
+        int height = 0;
 
-	// Get binary
-	private static String convertDecimalToBinary(int value) {
+        for (int i = 0; i < 9; i++) {
 
-		String binaryString = "0000000" + Integer.toBinaryString(value);
-		binaryString = binaryString.substring(binaryString.length() - 8);
+            int red = 0;
+            int green = 0;
+            int blue = 0;
+            int width = image.getWidth();
 
-		return binaryString;
-	}
+            // byte du tru
+            if (i == 1) {
+                continue;
+            }
 
-	// Convert binary to decimal
-	private static int convertBinaryToDecimal(String binary) {
+            for (height = milestones[i]; height < milestones[i + 1]; height++) {
 
-		int decimal = Integer.parseInt(binary, 2);
+                for (int j = 0; j < width; j++) {
+                    int rgbValue = image.getRGB(j, height);
 
-		return decimal;
-	}
+                    red += (rgbValue >> 16) & 0xff;
+                    green += (rgbValue >> 8) & 0xff;
+                    blue += rgbValue & 0xff;
+                }
+            }
 
-	// Write new image
-	public static void writeNewImage(BufferedImage image, String pathOutputNewImage) {
-		try {
-			File file = new File(pathOutputNewImage);
-			ImageIO.write(image, "png", file);
-		} catch (IOException e) {
-			System.out.println("Error write image: " + e);
-		}
-	}
+            // red max
+            if ((red > green && red > blue) || (red == blue && red > green)) {
+                keyK2 += "0";
+                continue;
+            }
+
+            // green max
+            if ((green > red && green > blue) || (red == green && red == blue)
+                    || (red == green && red > blue) || (blue == green && blue > red)) {
+                keyK2 += "1";
+                continue;
+            }
+
+            // blue max
+            if (blue > green && blue > red) {
+                keyK2 += "2";
+            }
+
+        }
+
+        return keyK2;
+    }
+
+    public static void getRGB(BufferedImage image, String binaryString) {
+
+        int[] milestones = divideImageTo8Blocks(image);
+
+        String keyK1 = createK1Key();
+        String keyK2 = findK2Key(image, milestones);
+
+        String[] blockBinaryCipherText = divideCipherTextTo8Blocks(binaryString);
+
+        int height = 0;
+        int width = image.getWidth();
+        int indexKey = 0;
+
+        for (int i = 0; i < 9; i++) {
+
+            // byte du tru
+            if (i == 1) {
+                // Embed K1 and K2
+
+                continue;
+            }
+
+            int indexKeyK1 = Integer.valueOf(String.valueOf(keyK1.charAt(indexKey)));
+            int indexKeyK2 = Integer.valueOf(String.valueOf(keyK2.charAt(indexKey)));
+
+            for (height = milestones[i]; height < milestones[i + 1]; height++) {
+
+                for (int j = 0; j < width; j++) {
+
+                    int rgbValue = image.getRGB(j, height);
+
+//                    int red = (rgbValue >> 16) & 0xff;
+//                    int green = (rgbValue >> 8) & 0xff;
+//                    int blue = rgbValue & 0xff;
+                    // Set new rgb
+                    int newRGBValue = setNewRGBValue(rgbValue, indexKeyK1, indexKeyK2, blockBinaryCipherText[indexKeyK1]);
+
+                    image.setRGB(j, i, newRGBValue);
+                }
+            }
+
+            indexKey++;
+
+        }
+
+    }
+
+    private static int setNewRGBValue(int rgbValue, int indexKeyK1, int indexKeyK2, String blockBinaryCipherText) {
+
+        // System.out.println("============= Set New RGB =======================");
+        int red = (rgbValue >> 16) & 0xff;
+        int green = (rgbValue >> 8) & 0xff;
+        int blue = rgbValue & 0xff;
+
+        // convert rgb value to binary
+        String redBinary = convertDecimalToBinary(red);
+        String greenBinary = convertDecimalToBinary(green);
+        String blueBinary = convertDecimalToBinary(blue);
+
+        // System.out.println("RedBinary : " + redBinary);
+        // System.out.println("GreenBinary: " + greenBinary);
+        // System.out.println("BlueBinary : " + blueBinary);
+        // System.out.println("============= Red: add CipherToBit ==============");
+        
+        if (indexKeyK2 == 0) {
+            int changeI = 0;
+            int green1 = 0;
+            int blue2 = 0;
+            
+            
+        }
+        
+        if (indexKeyK2 == 1) {
+            int changeI = 0;
+            int red1 = 0;
+            int blue2 = 0;
+            
+            
+        }
+        
+        if (indexKeyK2 == 2) {
+            int changeI = 0;
+            int red1 = 0;
+            int green2 = 0;
+            
+            
+        }
+        
+        rgbValue = (redChanged << 16) | (greenChanged << 8) | blueChanged;
+
+        return rgbValue;
+    }
+
+    // Get binary
+    private static String convertDecimalToBinary(int value) {
+
+        String binaryString = "0000000" + Integer.toBinaryString(value);
+        binaryString = binaryString.substring(binaryString.length() - 8);
+
+        return binaryString;
+    }
+
+    // Convert binary to decimal
+    private static int convertBinaryToDecimal(String binary) {
+
+        int decimal = Integer.parseInt(binary, 2);
+
+        return decimal;
+    }
+
+    // Write new image
+    public static void writeNewImage(BufferedImage image, String pathOutputNewImage) {
+        try {
+            File file = new File(pathOutputNewImage);
+            ImageIO.write(image, "png", file);
+        } catch (IOException e) {
+            System.out.println("Error write image: " + e);
+        }
+    }
 }
